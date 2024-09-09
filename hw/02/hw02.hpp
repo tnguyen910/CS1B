f5966ad59342d95f06732dc8af3c6ec31892e372
Tim Nguyen <tim@Tims-MacBook-Pro.local>
2024-09-08 21:00:35 -0700

    hw02

---
+++ hw/02/hw02.hpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+void userMenu();
+void showCupCount();
+void showMoneyMade();
+void showCoffeeSold();
+void sellCoffee();
---
+++ hw/02/hw02menu.cpp
@@ -0,0 +1,3 @@
+#include "hw02.hpp"
+
+void userMenu() { std::cout << "This is userMenu()" << std::endl; }
